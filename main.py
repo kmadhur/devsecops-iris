4185d09f43d07180f540d6d5656f0a149593e7ba
Madhuri <madhurihere1@gmail.com>
2021-09-27 15:01:21 +0530

    First commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from ml_utils import load_model, predict, retrain
+from ml_utils import load_model, predict , retrain
 from typing import List
 
 # defining the main app
